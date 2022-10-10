203e26fde57163a9b4b57e195b77462f0e23655b
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-10 10:37:09 +0000

    Mon, Oct 10, 2022, 10:37 AM +00:00

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 class Task:
-    def __init__(self, title, date, context, project, tags):
+    def __init__(self, title, date, context, project, priority, tags, note):
         self.title = title
         self.duedate = date
         self.context = context
         self.project = project
+        self.priority = priority
         self.tags = tags
+        self.note = note
     def rename(self, newtitle):
         self.title = newtitle
     def update(self, title, date, tags, project, context)
