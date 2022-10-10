9759171a56695afd2589f1097dfdba1d341d8b6c
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-10 08:13:56 +0000

    Mon, Oct 10, 2022, 8:13 AM +00:00

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 class Task:
     title = "Do the work"
     duedate = "2022-10-10"
+    def __init__(self, title):
+        self.title = title
     def rename(self, newtitle):
         self.title = newtitle
 
 task1 = Task()
 print(task1.title)
+task1.rename("New task title")
+print(task1.title)
 
-print(task1.rename("New task title").title)
+task2 = Task("init task creation")
+print(task2.title)
