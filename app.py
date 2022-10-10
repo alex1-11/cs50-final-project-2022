77b9966be6f9a5f044484298fcd7c7d93d38846c
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-10 14:29:43 +0000

    Mon, Oct 10, 2022, 2:29 PM +00:00

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 class Task:
-    def __init__(self, title, date=None, time=None, context=None, project=None, priority=None, tags=None, note=None, alarms=None, active=True):
+    def __init__(self, title, date=None, time=None, context=None,
+                 project=None, priority=None, tags=None, note=None,
+                 alarms=None, active=True):
         self.title = title
         self.date = date
         self.time = time
@@ -11,15 +13,9 @@ class Task:
         self.alarms = alarms
         self.active = active
 
-    def rename(self, newtitle):
-        self.title = newtitle
-
-    def update(self, title, date, tags, project, context)
-        self.
-
 class Project:
     def __init__(self, title, parent=None, level=0, active=True,
-    color='#FFFFFF'):
+                 color='#FFFFFF'):
         self.title = title
         self.parent = parent
         self.level = level
