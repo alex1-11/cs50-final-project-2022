382054176aa9b4eebb3d92e660493b17800a0b34
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-14 16:20:05 +0000

    Fri, Oct 14, 2022, 4:20 PM +00:00

--- models.py
+++ models.py
@@ -9,10 +9,30 @@ Base = declarative_base
 task_tags = Table(
     "task_tags",
     Base.metadata,
-    Column(")
+    Column("task_id", Integer, ForeignKey("tasks.id")),
+    Column("tag_id", Integer, ForeignKey("tags.id"))
 )
 
+user_settings = Table(
+    "user_settings",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("setting_id", Integer, ForeignKey("settings.id")),
+    Column("state", Integer)
+)
+
+class Users(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    hash = Column(String)
+    settings = relationship(
+        "Settings", secondary=user_settings, back_populates="users"
+    )
 
 
 
-user_settings
+
+
+
+
