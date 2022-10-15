71ff0dc9265af51230a090005e67e00d1c2f8623
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-15 17:54:42 +0000

    Sat, Oct 15, 2022, 5:54 PM +00:00

--- app.py
+++ app.py
@@ -18,6 +18,21 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure SQLAlchemy
+# TODO: Configure db for app using SQLAlchemy
 
-db =
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index()
+    """Show dashboard of today's tasks"""
+
+    # Get user
