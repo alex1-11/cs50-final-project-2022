847e33e0b0b7808c3b3088a49ed41cfe78a857f2
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-15 17:08:13 +0000

    Sat, Oct 15, 2022, 5:08 PM +00:00

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from models import User, Setting, Task, Project, Tag, Context, Alarm
+from models import User, Setting, Task, Project, Tag, Context, Alarm, task_tags, user_settings
 # from helpers import login_required, apology
 # from flask_sqlalchemy import SQLAlchemy
 
