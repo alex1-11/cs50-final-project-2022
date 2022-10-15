9602a8a9e09d240ef3f9186dc00baa4cc0d9a592
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-15 17:13:00 +0000

    Sat, Oct 15, 2022, 5:13 PM +00:00

--- app.py
+++ app.py
@@ -7,3 +7,14 @@ from models import User, Setting, Task, Project, Tag, Context, Alarm, task_tags,
 # from flask_sqlalchemy import SQLAlchemy
 
 
+# Configure application (thanks to CS50 Finance problemset)
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Cofigure session to use filesystem (intead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
