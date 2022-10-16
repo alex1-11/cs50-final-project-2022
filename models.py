f449cf07348841ce765e6cedde8782cc5b7f8feb
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-16 11:41:57 +0000

    Sun, Oct 16, 2022, 11:41 AM +00:00

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from app import db
 # https://realpython.com/python-sqlite-sqlalchemy/#working-with-sqlalchemy-and-python-objects
 
 Base = declarative_base
@@ -80,7 +81,7 @@ class Alarm(Base):
     id = Column(Integer, primary_key=True)
     task_id = Column(Integer, ForeignKey("tasks.id"))
     datetime = Column(String)
-    
+
 
 
     # TODO: How to set constraint on columns?
