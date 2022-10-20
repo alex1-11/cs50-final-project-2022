6413e6148fc23fb06621993411394444d402150c
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 08:25:00 +0000

    Thu, Oct 20, 2022, 8:25 AM +00:00

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 const tasks_data = {{ tasks|tojson}}
 
 // if ADD NEW TASK
-let task_new = new FormData()
+let task_new = new FormData(document.querySelector("#add_new_task"))
 task_new.append('title': )
 fetch('/')
     .then(response => response.json())
