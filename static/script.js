31d0613382b72898af35492ac0a70e8e2eb677cb
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 11:52:32 +0000

    Thu, Oct 20, 2022, 11:52 AM +00:00

--- static/script.js
+++ static/script.js
@@ -3,7 +3,6 @@
 // if ADD NEW TASK
 
 function addtask(event) {
-    event.preventDefault();
     let task_new = new FormData(document.querySelector("#add_new_task"));
     const tasklist_end_div = getElementById("tasklist_end");
     fetch('/', {
@@ -11,6 +10,7 @@ function addtask(event) {
         "body": task_new,
     }).then(response => response.text)
     .then(text => tasklist_end_div.inserAdjacentHTML("beforebegin", text))
+    event.preventDefault();
 };
 
 document.querySelector("#task_new_trigger").addEventListener("submit", addtask);
