99f16cc3550d7a3cddbdd5ce89b0b38a81e49ad0
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 09:39:12 +0000

    Thu, Oct 20, 2022, 9:39 AM +00:00

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def index():
                 )
                 print(task_new.__dict__)
                 db.add(task_new)
-                db.refresh(task_new)
+                db.flush()
                 return render_template("test.html", task=task_new)
             # return redirect("/")
 
