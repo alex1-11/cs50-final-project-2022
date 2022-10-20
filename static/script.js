aba36f1bb8bd1a2c4b05b885b5182965709646d2
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 10:07:52 +0000

    Thu, Oct 20, 2022, 10:07 AM +00:00

--- static/script.js
+++ static/script.js
@@ -1,17 +1,16 @@
-const tasks_data = {{ tasks|tojson}}
+const tasks_data = {{ tasks|tojson}};
 
 // if ADD NEW TASK
 
+querySelector("#task_new_trigger").addEventListener("click", addtask());
 function addtask() {
-    event.preventDefault()
-    let task_new = new FormData(document.querySelector("#add_new_task"))
-    const tasklist_end_div = getElementById("tasklist_end")
+    event.preventDefault();
+    let task_new = new FormData(document.querySelector("#add_new_task"));
+    const tasklist_end_div = getElementById("tasklist_end");
     fetch('/', {
         "method": "POST",
         "body": task_new,
     }).then(response => response.text)
     .then(text => tasklist_end_div.inserAdjacentHTML("beforebegin", text)
     )
-}
-
-querySelector("#task_new_trigger").addEventListener("click", addtask())
+};
