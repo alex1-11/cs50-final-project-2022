7c634df734cc990aeb55ef6d81d5bdc8d9ea17d3
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 15:53:51 +0000

    Thu, Oct 20, 2022, 3:53 PM +00:00

--- static/script.js
+++ static/script.js
@@ -22,8 +22,8 @@ form_add_new_task.addEventListener("submit", add_new_task)
 
 
 // DELETE TASK TO BIN
-function task_delete(event) {
-    const delete_trigger_div
+function task_delete(event, this) {
+    const delete_trigger_div = document.querySelector("#del_task_id_${this.value}")
 
 
 
