e6588c3b41adcf039ca50d51f97619d3b73265d4
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-20 08:29:37 +0000

    Thu, Oct 20, 2022, 8:29 AM +00:00

--- static/script.js
+++ static/script.js
@@ -2,8 +2,10 @@ const tasks_data = {{ tasks|tojson}}
 
 // if ADD NEW TASK
 let task_new = new FormData(document.querySelector("#add_new_task"))
-fetch('/')
-    .then(response => response.json())
+fetch('/', {
+    "method": "POST",
+    "body": task_new,
+}).then(response => response.json())
     .then(data => {
         ??
     })
