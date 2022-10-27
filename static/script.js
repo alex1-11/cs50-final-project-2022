7fa7bd055cb58f6946c30e2fa5ac56ce2c25d66e
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-27 15:47:25 +0000

    Thu, Oct 27, 2022, 3:47 PM +00:00

--- static/script.js
+++ static/script.js
@@ -74,6 +74,6 @@ task_divs.forEach(div => task_set_triggers(div))
 
 // TODO: Tasklists / views
 // const tasks_data = {{ tasks|tojson }}
-
+let view = 'today'
 
 // May get handy (call func/var from string name) https://www.geeksforgeeks.org/how-to-call-function-from-it-name-stored-in-a-string-using-javascript/
