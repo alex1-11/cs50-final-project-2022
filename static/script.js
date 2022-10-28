6f99a91290c327c987930b06e35fdce4352a7798
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-28 16:42:03 +0000

    Fri, Oct 28, 2022, 4:42 PM +00:00

--- static/script.js
+++ static/script.js
@@ -126,7 +126,9 @@ function update_active_tab() {
     }
 }
 
-tab_btns.children.forEach(btn => btn.addEventListener('click', update_active_tab(), false))
+tab_btns.children.forEach(btn => {
+    btn.addEventListener('click', update_active_tab(), false)
+})
 
 
 // TODO: set due date / datetime
