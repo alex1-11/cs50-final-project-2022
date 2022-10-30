8623d42152a8357367eebb7488a594499eae3a57
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-10-30 18:56:29 +0000

    Sun, Oct 30, 2022, 6:56 PM +00:00

--- app.py
+++ app.py
@@ -258,6 +258,9 @@ def index():
         # TODO: Get user's tasks, grouped by contexts
         # TODO: Views. Use JS, fetch and JSON:
         # https://flask.palletsprojects.com/en/2.2.x/patterns/javascript/
+        view = request.args.get("view")
+        match view:
+            case 'today':
 
         # Connect to db, load up tasks and show them out
         with DbSession.begin() as db:
@@ -280,6 +283,3 @@ def index():
             return render_template("index.html", tasks=tasks)
 
 
-    view = request.args.get("view")
-    match view:
-        case 'today':
