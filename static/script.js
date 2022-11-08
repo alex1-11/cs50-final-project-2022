f22f1496f4d6297b839a9b3d840416b3fa1ef9e2
alex1-11 <108358127+alex1-11@users.noreply.github.com>
2022-11-08 16:32:51 +0000

    Tue, Nov 8, 2022, 4:32 PM +00:00

--- static/script.js
+++ static/script.js
@@ -148,9 +148,10 @@ viewlist_btns.forEach(btn => btn.addEventListener('click', viewChange, false))
 // Show form to change task's title
 taskEditTitleToggle() {
     var task_div = document.querySelector(`#task_id_${this.value}`)
-    if (this.name == )
+    switch (this.name == task_title_edit_show)
     task_div.querySelector('.task_div_edit_title').classList.remove('d-none')
     task_div.querySelector('.task_title_text').classList.add('d-none')
+    task_title_edit_hide
 }
 
 
